3e0f79f063ce9bbf045271d8337d7ae4ea1883a6
aleksandr.evseev <sanevs@bk.ru>
2023-09-18 23:37:30 +0300

    start

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductModule } from './product/product.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'product',
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: false,
+    }),
+    ProductModule,
+  ],
+})
+export class AppModule {}
